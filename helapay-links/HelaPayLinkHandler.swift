0035c3108ea1546c4283cf99e7d7740ed17553a0
Thisura98 <thisura1998@gmail.com>
2022-02-09 12:58:08 +0530

    Add fallback to default open on request error

--- helapay-links/HelaPayLinkHandler.swift
+++ helapay-links/HelaPayLinkHandler.swift
@@ -31,9 +31,15 @@ final class HelaPayLinkHandler{
         let request = URLRequest(url: url)
         
         HelaPayLinkHandler.session.dataTask(with: request) { _, res, err in
-            guard err == nil, let cres = res as? HTTPURLResponse else { return }
             DispatchQueue.main.async {
-                print(cres.allHeaderFields)
+                guard err == nil, let cres = res as? HTTPURLResponse else {
+                    #if DEBUG
+                    print("handleHelaPayLink", err ?? "Unknown error occurred")
+                    #endif
+                    UIApplication.shared.open(url, options: [:], completionHandler: nil)
+                    return
+                }
+                
                 if let link = cres.allHeaderFields["Location"] as? String, let deepLink = URL(string: link){
                     UIApplication.shared.open(deepLink, options: [:], completionHandler: nil)
                 }
